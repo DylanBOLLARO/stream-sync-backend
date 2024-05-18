71c5fcb35aa8d037463728e0be10b3fd344be52c
Dylan <bollarodylan@gmail.com>
2024-05-18 18:02:18 +0200

    remove /api prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix('/api/v1');
+	app.setGlobalPrefix('/v1');
 
 	app.enableCors({
 		origin: true,
